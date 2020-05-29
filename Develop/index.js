ee9e166c3b295f562899359149c8680ca041784e
Skyler Bluth <webdevelopmentdiva@gmail.com>
2020-05-29 08:01:12 -0700

    push working remote

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,80 @@
-const questions = [
+const axios = require('axios');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const questions = [{
+        name: 'username',
+        type: 'input',
+        message: 'What is your github username?'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'What is your email address?'
+    },
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is your project title?'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Describe your project'
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'Step-by-Step installation process'
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'How can we implement this project?'
+    },
+    {
+        name: 'license',
+        type: 'input',
+        message: "What is your project's license?"
+    },
+    {
+        name: 'contributions',
+        type: 'input',
+        message: 'Who contributed to your project?'
+    },
+    {
+        name: 'testing',
+        type: 'input',
+        message: 'How have you tested your project?'
+    },
+    {
+        name: 'url',
+        type: 'input',
+        message: 'What is your project URL?'
+    },
 
 ];
 
-function writeToFile(fileName, data) {
+function gitHub(username) {
+    let profPictUrl;
+
+    return axios
+        .get(`https://api.github.com/users/${username}/repos`)
+        .then((res) => {
+            profPictUrl = res;
+            console.log(profPicUrl)
+        })
+        .catch((err) => {
+            console.log(`Error: invalid Github username`);
+        })
 }
 
-function init() {
+gitHub();
 
-}
 
-init();
+inquirer
+    .prompt()
+    .then(() {
+
+    })
